6d2c765f68c1abdf1c150116cf905aa21a728977
Giuditta <giudittamarino89@gmail.com>
2021-03-12 17:31:10 +0100

    inizio main.js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  var dropdown = $('header-right ul li.drop');
+
+  // AL CLICK SULL'LI
+  dropdown.click(function() {
+    console.log('ok');
+  })
+    // DEVO MOSTRARE IL DROPDOWN MENU
+
+
+
+
+
+
+
+
+})
